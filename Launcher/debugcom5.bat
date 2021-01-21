141b2ccefe35c99898580b9aa13dc9da93616180
iGlitch <iglitch4lolz@yahoo.co.jp>
2021-01-21 00:02:49 -0500

    Cleanup
    for this branch

--- Launcher/debugcom5.bat
+++
@@ -1,4 +0,0 @@
-set WIILOAD=COM5
-C://devkitPro//devkitPPC//bin//wiiload boot.elf
-start C://devkitPro//insight//bin//powerpc-eabi-insight.exe boot.elf
-start C://devkitPro//insight//bin//powerpc-eabi-gdb.exe boot.elf target remote COM5
